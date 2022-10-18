cde8184f86de8903905711f1c487a1447bbbc542
Skater Punisher <skaterpunisher@gmail.com>
2022-10-18 22:17:42 +0300

    add decrementButton

--- src/ui/Button/DecrementGoodsBtn/DecrementGoodsBtn.jsx
+++ src/ui/Button/DecrementGoodsBtn/DecrementGoodsBtn.jsx
@@ -1,10 +1,10 @@
-
-import { MdRemove } from "react-icons/md";
+import { MdRemove } from 'react-icons/md';
 import { message } from 'antd';
 import { useSelector } from 'react-redux';
 import {
   useGetSingleUserQuery,
   useIncrementGoodsInBasketMutation,
+  useRemoveGoodsInBasketMutation,
 } from '../../../redux/goodsApi';
 import Spinner from '../../Spinner/Spinner';
 
@@ -18,29 +18,53 @@ const DecrementGoodsBtn = ({ id }) => {
   const [addProductInBasket, { isLoading }] =
     useIncrementGoodsInBasketMutation();
 
+  const [RemoveProductInBasket] =
+    useRemoveGoodsInBasketMutation();
+
   const handleDecrementColGoodsInBasket = async () => {
-    let indexItem = data.basket.item.findIndex(item => item.id == id)
-
+    let indexItem = data.basket.item.findIndex((item) => item.id == id);
     let basketUser = [...data.basket.item.filter((item) => item.id != id)];
-    let targetGoods = {...data.basket.item.filter((item) => item.id == id)[0]};
-    targetGoods.col = targetGoods.col + 1
+    let targetGoods = {
+      ...data.basket.item.filter((item) => item.id == id)[0],
+    };
+    if (targetGoods.col > 1) {
+      targetGoods.col = targetGoods.col - 1;
 
-    let newBasketUser = [...basketUser]
-    newBasketUser.splice(indexItem, 0, targetGoods)
+      let newBasketUser = [...basketUser];
+      newBasketUser.splice(indexItem, 0, targetGoods);
 
-    successAdd();
-    await addProductInBasket({
-      idUser: user.id,
-      item: newBasketUser,
-      generalSum: data.GeneralsumInBasket + targetGoods.price,
-    }).unwrap();
+      let generalSum = data.GeneralsumInBasket - targetGoods.price;
+      successAdd();
+
+      await addProductInBasket({
+        idUser: user.id,
+        item: newBasketUser,
+        generalSum: generalSum,
+      }).unwrap();
+    } else {
+      let result = data.basket.item.filter((item) => item.id != id);
+      let targetGoods = data.basket.item.filter((item) => item.id == id);
+      console.log(targetGoods);
+      let sum =
+        data.GeneralsumInBasket - targetGoods[0].price * targetGoods[0].col;
+      await RemoveProductInBasket({
+        id: data.id,
+        data: result,
+        sum: sum,
+      }).unwrap();
+    }
   };
 
   if (isLoading) return <Spinner />;
 
   return (
-    <button onClick={handleDecrementColGoodsInBasket} className='mr-2 border-2 p-2 rounded-3xl bg-gray-200 hover:bg-gray-400 hover:text-white duration-200'><MdRemove /></button>
-  )
-}
+    <button
+      onClick={handleDecrementColGoodsInBasket}
+      className='mr-2 border-2 p-2 rounded-3xl bg-gray-200 hover:bg-gray-400 hover:text-white duration-200'
+    >
+      <MdRemove />
+    </button>
+  );
+};
 
-export default DecrementGoodsBtn
+export default DecrementGoodsBtn;
