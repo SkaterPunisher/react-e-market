98fe89be5c00bd9e7edca8f055a314b19b51fb0c
Skater Punisher <skaterpunisher@gmail.com>
2022-10-21 09:51:07 +0300

    fixed history

--- src/components/HistoryUser/HistoryUser.jsx
+++ src/components/HistoryUser/HistoryUser.jsx
@@ -1,19 +1,63 @@
+import { CaretRightOutlined } from '@ant-design/icons';
+import { Collapse } from 'antd';
 import React from 'react';
+import MainBasketItem from '../BasketItem/MainBasketItem/MainBasketItem';
+import { v4 as uuidv4 } from 'uuid';
+import { Link } from 'react-router-dom';
+
+const { Panel } = Collapse;
 
 const HistoryUser = ({ data }) => {
   const { history } = data;
-  let arr = [];
-  for (let key in data.history) {
-    arr.push({
-      [key]: data.history[key],
-    });
-  }
-  console.log(history);
+  const arrHistory = Object.entries(history);
+  console.log(arrHistory);
+
+  if (arrHistory.length == 0) return <div className='text-center text-[25px]'>Истории заказов пока нет. <Link to='/goods'>Перейти к товарам</Link></div>;
 
   return (
     <div className='min-h-[250px] p-6'>
-      <div>Колличество товаров в корзине: {data.basket.item.length}</div>
-
+      {arrHistory.map((item) => {
+        return (
+          <Collapse
+            key={uuidv4()}
+            bordered={true}
+            defaultActiveKey={['0']}
+            expandIcon={({ isActive }) => (
+              <CaretRightOutlined rotate={isActive ? 90 : 0} />
+            )}
+            className='site-collapse-custom-collapse'
+          >
+            <Panel
+              header={item[0]}
+              key='1'
+              className='site-collapse-custom-panel'
+            >
+              {Object.entries(item[1]).map((elem) => {
+                const { id, title, price, img, col } = elem[1];
+                let resultSum = col * price;
+                return (
+                  <div
+                    className='flex items-center mb-4 justify-between'
+                    key={uuidv4()}
+                  >
+                    <MainBasketItem
+                      id={id}
+                      title={title}
+                      price={price}
+                      img={img}
+                      col={col}
+                    />
+                    <div className='min-w-[200px]'>
+                      <h2>{col} шт</h2>
+                      <h2>Всего: {resultSum} ₽</h2>
+                    </div>
+                  </div>
+                );
+              })}
+            </Panel>
+          </Collapse>
+        );
+      })}
     </div>
   );
 };
--- src/pages/LkPage.jsx
+++ src/pages/LkPage.jsx
@@ -16,36 +16,34 @@ const LkPage = () => {
 
   return (
     <div>
-      <div className='flex'>
-        <div className='w-[30%]'>
-          <ul className='px-6 py-2 bg-gray-800 h-full flex flex-col justify-center'>
-            <li
-              className={`mb-6 ${
-                location.pathname == '/lk/history'
+      <div className='text-[40px] text-center'>
+        Добро пожаловать {data.name} !
+      </div>
+      <div className='flex flex-col'>
+        <ul className='px-6 py-2 bg-gray-800 h-full flex flex-col justify-start'>
+          <li
+            className={`mb-6 ${
+              location.pathname == '/lk/history'
+                ? 'bg-gray-900 text-white text-[20px] px-4 py-2 rounded-lg mx-2'
+                : 'text-gray-300 text-[20px] hover:bg-gray-700 hover:text-white px-4 py-2 rounded-lg mx-2'
+            }`}
+          >
+            <Link to='history'>История заказов</Link>
+          </li>
+          <li>
+            <Link
+              className={`${
+                location.pathname == '/lk/about'
                   ? 'bg-gray-900 text-white text-[20px] px-4 py-2 rounded-lg mx-2'
                   : 'text-gray-300 text-[20px] hover:bg-gray-700 hover:text-white px-4 py-2 rounded-lg mx-2'
               }`}
+              to='about'
             >
-              <Link to='history'>История заказов</Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  location.pathname == '/lk/about'
-                    ? 'bg-gray-900 text-white text-[20px] px-4 py-2 rounded-lg mx-2'
-                    : 'text-gray-300 text-[20px] hover:bg-gray-700 hover:text-white px-4 py-2 rounded-lg mx-2'
-                }`}
-                to='about'
-              >
-                Информация о пользователе
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className='w-[70%]'>
-          <div className='text-[40px] text-center'>
-            Добро пожаловать {data.name} !
-          </div>
+              Информация о пользователе
+            </Link>
+          </li>
+        </ul>
+        <div className=''>
           <Routes>
             <Route path='history' element={<HistoryUser data={data} />} />
             <Route path='about' element={<AboutUser data={data} />} />
