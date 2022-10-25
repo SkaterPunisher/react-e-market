9b12e54f2bc88f778e05d77d7de54f87f2f4fe9e
Skater Punisher <skaterpunisher@gmail.com>
2022-10-25 08:32:20 +0300

    add burgerMenu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className='bg-gray-800 mb-2'>
       <div className='h-[80px] flex justify-between items-center max-w-[1280px] mx-auto px-10'>
-        <div className='text-white text-[20px] cursor-default'>E-commerce</div>
+        <div className='text-white text-[20px] cursor-default hidden md:block'>E-commerce</div>
         <NavLinks />
         <div className='flex'>
           <SwitchTheme />
--- src/components/Header/NavLinks/NavLinks.jsx
+++ src/components/Header/NavLinks/NavLinks.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import CustomLink from '../../CustomLink/CustomLink';
+import { MdMenu, MdClose } from 'react-icons/md';
 
 const NavLinks = () => {
   const auth = useSelector((state) => state.users.auth);
+  const [visible, setVisible] = useState(false);
 
   return (
-    <div className=''>
-      <CustomLink to='/' >
-        Главная
-      </CustomLink>
-      <CustomLink to='/goods'>
-        Товары
-      </CustomLink>
-      {auth ? (
-        <CustomLink to='/admin'>
-          Админка
-        </CustomLink>
-      ) : (
-        ''
-      )}
-    </div>
+    <>
+      <div className='sm:hidden'>
+        <MdMenu
+          onClick={() => setVisible(!visible)}
+          className='text-white w-[35px] h-[35px] cursor-pointer'
+        />
+        <div
+          className={`duration-300 ${
+            visible
+              ? 'absolute top-0 left-0 w-[320px] h-full bg-gray-800'
+              : 'absolute left-[-320px]'
+          }`}
+        >
+          <MdClose
+            onClick={() => setVisible(!visible)}
+            className='absolute text-white w-[35px] h-[35px] cursor-pointer mt-[20px] ml-[260px]'
+          />
+          <div className='text-white text-[20px] cursor-default mt-6 ml-4'>
+            E-commerce
+          </div>
+          <div className='flex flex-col mt-8'>
+            <CustomLink to='/'>Главная</CustomLink>
+            <CustomLink to='/goods'>Товары</CustomLink>
+            {auth ? <CustomLink to='/admin'>Админка</CustomLink> : ''}
+          </div>
+        </div>
+      </div>
+      <div className='hidden sm:block'>
+        <CustomLink to='/'>Главная</CustomLink>
+        <CustomLink to='/goods'>Товары</CustomLink>
+        {auth ? <CustomLink to='/admin'>Админка</CustomLink> : ''}
+      </div>
+    </>
   );
 };
 
