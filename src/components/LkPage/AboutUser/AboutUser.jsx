366eb7cdff2d9795f49ada6bb56b47492b11b9f5
Skater Punisher <skaterpunisher@gmail.com>
2022-10-25 13:06:36 +0300

    fixed

--- src/components/LkPage/AboutUser/AboutUser.jsx
+++ src/components/LkPage/AboutUser/AboutUser.jsx
@@ -6,11 +6,10 @@ const AboutUser = ({ data }) => {
   return (
     <div className='flex sm:flex-row items-center sm:justify-center flex-col-reverse'>
       <div>
-        <h2 className='text-[16px] dark:text-white'>Email: <span className='text-gray-400'>{email}</span></h2>
-        <h2 className='text-[16px] dark:text-white'>Name: <span className='text-gray-400'>{name}</span></h2>
-        <h2 className='text-[16px] dark:text-white'>Password: <span className='text-gray-400'>{password}</span></h2>
-        <h2 className='text-[16px] dark:text-white'>Role: <span className='text-gray-400'>{role}</span></h2>
-        <h2 className='text-[16px] dark:text-white'>Email: <span className='text-gray-400'>{email}</span></h2>
+        <h2 className='text-[16px] dark:text-white'>Почта: <span className='text-gray-400'>{email}</span></h2>
+        <h2 className='text-[16px] dark:text-white'>Имя: <span className='text-gray-400'>{name}</span></h2>
+        <h2 className='text-[16px] dark:text-white'>Пароль: <span className='text-gray-400'>{password}</span></h2>
+        <h2 className='text-[16px] dark:text-white'>Тип: <span className='text-gray-400'>{role}</span></h2>
       </div>
       <img src={avatar} alt={name} className='w-[150px] h-[150px] object-cover sm:ml-20 mb-4 sm:mb-0 rounded-full' />
     </div>
